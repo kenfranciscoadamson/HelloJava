b21441fd04894b5933ddbfe74f89215cdd754752
Ken Francisco <jameskenfrancisco@gmail.com>
2020-03-27 10:09:32 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		String name = "Ken";
+		
+		System.out.println("Hello World!");
+		//println prints with new line
+		//while print only prints
+		// \n gives a newline
+		System.out.println("\n..and hello to you, " + name +"!");
+
+		//comment
+		/*
+		multi-line comments
+		*/
+	}
+}
